a004e0bddae6a7d1923db7feb385f76c48e77524
Abid Ali <49807758+Abidali08@users.noreply.github.com>
2025-04-08 15:34:36 +0000

    Create structure

---
+++ Estore/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estore.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string Password { get; set; } = null!;
+}
---
+++ Estore/Models/NeondbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estore.Models;
+
+public partial class NeondbContext : DbContext
+{
+    public NeondbContext()
+    {
+    }
+
+    public NeondbContext(DbContextOptions<NeondbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseNpgsql("Host=ep-wild-sound-a5tph2xr-pooler.us-east-2.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_zcwdA3oUI0Wa");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("customer_pkey");
+
+            entity.ToTable("customer");
+
+            entity.HasIndex(e => e.Email, "customer_email_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .HasColumnName("email");
+            entity.Property(e => e.Name)
+                .HasMaxLength(28)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(16)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
